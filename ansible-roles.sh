ce6be478973ffaeb3223746738847b71b1453397
Diox <gt@ks.diox.es>
2020-11-01 21:35:47 +0000

    Fixed Crypto to collection

--- ansible-roles.sh
+++ ansible-roles.sh
@@ -1,3 +1,3 @@
 ansible-galaxy collection install community.digitalocean
-ansible-galaxy install community.crypto
+ansible-galaxy collection install community.crypto
 ansible-galaxy install elastic.elasticsearch,7.9.2
